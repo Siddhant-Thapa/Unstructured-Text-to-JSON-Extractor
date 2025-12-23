4f6a26218b73fa627a232e24cc7521443aa8c196
Siddhant-Thapa <thapasiddhant9@gmail.com>
2025-12-24 00:02:26 +0530

    feat: normalize currency symobls to IOS codes in invoice extraction

--- backend/app/validator.py
+++ backend/app/validator.py
@@ -1,15 +1,28 @@
 import json
 
-def validate_and_merge(schema:dict, llm_output: str) -> dict:
+CURRENCY_MAP = {
+    "₹": "INR",
+    "$": "USD",
+    "€": "EUR",
+    "£": "GBP"
+}
+
+
+def validate_and_merge(schema: dict, llm_output: str) -> dict:
+   
     try:
         parsed = json.loads(llm_output)
     except json.JSONDecodeError:
-        raise ValueError("LLM returned invalid json")
-    
+        raise ValueError("LLM returned invalid JSON")
+
     result = schema.copy()
 
     for key in result.keys():
         if key in parsed:
             result[key] = parsed[key]
-    
+
+    # Normalize currency symbol to ISO code if present
+    if "currency" in result and result["currency"] in CURRENCY_MAP:
+        result["currency"] = CURRENCY_MAP[result["currency"]]
+
     return result
